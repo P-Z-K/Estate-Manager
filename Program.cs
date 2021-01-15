aeaaefd9480b50e2f6040b2c499e0d6375e9ee50
P-Z-K <michal.niemczykit@gmail.com>
2021-01-15 15:32:13 +0100

    Add program's structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstateManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Utils/EstatePrinter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstateManager.Utils
+{
+    class EstatePrinter
+    {
+    }
+}
